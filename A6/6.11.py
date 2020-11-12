fb0078704c5f1556f6001a12aa6e5b8a1bf65dd7
Jorge Acosta <66733491+jorgeacosta19@users.noreply.github.com>
2020-11-12 19:37:40 +0100

    Add files via upload

---
+++ A6/6.11.py
@@ -0,0 +1,2 @@
+#11 - Write a Python program to find three numbers from an array such that the 
+# sum of three numbers equal to zero
---
+++ A6/6.12.py
@@ -0,0 +1,5 @@
+#12 - Write a Python program to reverse the digits of an integer.
+number=int(input("Please type a digit: "))
+print(str(number)[::-1])
+
+
